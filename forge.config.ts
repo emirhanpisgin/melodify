fe0e7b4e92cbad413789158d1d1d6b36d552a5c3
emirhanpisgin <emirhanpgt@gmail.com>
2025-06-29 23:51:58 +0300

    chore: update package.json to simplify make command and add squirrel maker dependency

--- forge.config.ts
+++ forge.config.ts
@@ -1,4 +1,5 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
@@ -20,7 +21,8 @@ const config: ForgeConfig = {
     },
     rebuildConfig: {},
     makers: [
-        new MakerZIP({}, ["darwin", "win32"]),
+        new MakerSquirrel({}),
+        new MakerZIP({}, ["darwin"]),
         new MakerRpm({}),
         new MakerDeb({}),
     ],
