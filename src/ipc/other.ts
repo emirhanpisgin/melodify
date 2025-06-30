0ef4ebbae6a132e6e9ee9b1d4150c91fdb56c565
emirhanpisgin <emirhanpgt@gmail.com>
2025-07-01 00:06:46 +0300

    refactor: remove electron-updater dependency and streamline auto-update logic

--- src/ipc/other.ts
+++ src/ipc/other.ts
@@ -1,5 +1,4 @@
-import { app, BrowserWindow, ipcMain, shell } from "electron";
-import { autoUpdater } from "electron-updater";
+import { app, autoUpdater, BrowserWindow, ipcMain, shell } from "electron";
 import Config from "../lib/config";
 
 ipcMain.on("open-external", (_, url: string) => {
@@ -35,26 +34,16 @@ ipcMain.handle("update:getAuto", () => autoUpdateEnabled);
 ipcMain.handle("app:getVersion", () => app.getVersion());
 
 app.on("ready", () => {
-    if (autoUpdateEnabled) {
-        autoUpdater.autoDownload = true;
-        autoUpdater.checkForUpdatesAndNotify();
-    } else {
-        autoUpdater.autoDownload = false;
-    }
+    autoUpdater.checkForUpdates();
 });
 
 autoUpdater.on("checking-for-update", () => broadcastUpdateStatus("checking"));
-autoUpdater.on("update-available", (info: any) =>
-    broadcastUpdateStatus("available", { info })
-);
-autoUpdater.on("update-not-available", (info: any) =>
-    broadcastUpdateStatus("not-available", { info })
+autoUpdater.on("update-available", () => broadcastUpdateStatus("available"));
+autoUpdater.on("update-not-available", () =>
+    broadcastUpdateStatus("not-available")
 );
 autoUpdater.on("error", (err: any) =>
     broadcastUpdateStatus("error", { error: err.message })
-);
-autoUpdater.on("download-progress", (progress: any) =>
-    broadcastUpdateStatus("downloading", { percent: progress.percent })
 );
 autoUpdater.on("update-downloaded", (info: any) =>
     broadcastUpdateStatus("downloaded", { info })
